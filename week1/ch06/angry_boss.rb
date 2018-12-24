e6e14242bbf97dd927ae174099eac3a7d264f222
nazgus <gus@deleon.org.mx>
2018-12-23 18:49:55 -0800

    Week 1

---
+++ week1/ch06/angry_boss.rb
@@ -0,0 +1,3 @@
+puts 'WHADDAYA WANT?!? CAN\'T YOU SEE I\'M BUSY?!?'
+request = gets.chomp
+puts 'WHAT DO YOU MEAN"' + request.upcase + '"?!? YOU\'RE FIRED!!'
---
+++ week1/ch06/table_of_contents.rb
@@ -0,0 +1,12 @@
+l_width = 100
+puts 'Table of Contents'.center(l_width)
+puts ''
+puts 'Chapter 1: Getting Started'.ljust(l_width/2) + 'page  1'.rjust(l_width/2)
+puts 'Chapter 2: Numbers'.ljust(l_width/2) + 'page  9'.rjust(l_width/2)
+puts 'Chapter 3: Letters'.ljust(l_width/2) + 'page 13'.rjust(l_width/2)
+puts 'Chapter 4: Variables and Assignment'.ljust(l_width/2) + 'page 17'.rjust(l_width/2)
+puts 'Chapter 5: Mixing It Up'.ljust(l_width/2) + 'page 21'.rjust(l_width/2)
+puts 'Chapter 6: More About Methods'.ljust(l_width/2) + 'page 27'.rjust(l_width/2)
+puts 'Chapter 7: Flow Control'.ljust(l_width/2) + 'page 37'.rjust(l_width/2)
+puts 'Chapter 8: Arrays and Iterators'.ljust(l_width/2) + 'page 51'.rjust(l_width/2)
+puts 'Chapter 9: Writing Your Own Methods'.ljust(l_width/2) + 'page 57'.rjust(l_width/2)
